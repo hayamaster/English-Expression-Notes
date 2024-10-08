ad3bd7788e95a91d03fc46ac7358d6cca41f9939
hayamaster <dnfxmfkdbzos@naver.com>
2024-10-07 23:58:03 -0400

    style: list item style change

--- frontend/src/components/ListItem.js
+++ frontend/src/components/ListItem.js
@@ -1,10 +1,31 @@
 import { Link } from "react-router-dom";
 
+const getTime = (note) => {
+  return new Date(note.updated).toLocaleDateString();
+};
+
+const getTitle = (note) => {
+  const title = note.body.split("\n")[0];
+
+  return title;
+};
+
+const getContent = (note) => {
+  const title = getTitle(note);
+  const content = note.body.replaceAll("\n", " ").replaceAll(title, "");
+
+  return content;
+};
+
 const ListItem = ({ note }) => {
   return (
     <Link to={`note/${note.id}`}>
       <div className="notes-list-item">
-        <h3>{note.body}</h3>
+        <h3 className="title">{getTitle(note)}</h3>
+        <p>
+          <span>{getTime(note)}</span>
+          <span className="content">{getContent(note)}</span>
+        </p>
       </div>
     </Link>
   );
