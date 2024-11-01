f71594234c4d2c7d43b476a7293d57ad4668c0f2
hayamaster <dnfxmfkdbzos@naver.com>
2024-11-01 15:22:57 -0400

    chore: modify setting file

--- notes/settings.py
+++ notes/settings.py
@@ -113,7 +113,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'frontend/build/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'frontend/build/static',
